28d12da031d8b7d192b0cb1f365d1d2a309aff18
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-05-29 20:39:34 -0400

    Installed cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 let persons = [
   {
@@ -26,6 +27,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
@@ -102,4 +104,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// time spent: 5 hrs
+// time spent: 9 hrs
