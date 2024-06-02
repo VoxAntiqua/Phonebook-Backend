c295df47ee42675dc5fd831ff48cb475f7a8a50d
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-06-02 13:48:17 -0400

    Used Prettier to match code style to Full Stack Open's example code

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This application is an exercise for Full Stack Open offered through the University of Helsinki.
 
+Exercise 3.22 instructs to install ESLint, but since I had already been using Prettier, I changed Prettier's configuration to match the style in Full Stack Open's example code instead.
+
 The deployed version can be found at https://voxantiqua-phonebook.fly.dev
--- models/person.js
+++ models/person.js
@@ -1,44 +1,44 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    .connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    validate: {
-      validator: (v) => /^\d+-\d+$/.test(v),
-      message:
-        "number must consist of two groups of digits separated by a hyphen (-)",
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
     },
-    required: true,
-  },
-});
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: v => /^\d+-\d+$/.test(v),
+            message:
+                'number must consist of two groups of digits separated by a hyphen (-)',
+        },
+        required: true,
+    },
+})
 
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
