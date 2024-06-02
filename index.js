7021b430f047b2df0a67e36735f9d69e11e3ec4d
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-06-02 10:31:49 -0400

    Implemented delete person route with MongoDB

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
-
-/* let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-]; */
 
 app.use(express.json());
 app.use(express.static("dist"));
@@ -53,10 +31,6 @@ app.get("/api/persons/:id", morgan("tiny"), (request, response) => {
   });
 });
 
-/* const generateId = () => {
-  return Math.floor(Math.random() * 1000000);
-};
- */
 app.post(
   "/api/persons",
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
@@ -75,17 +49,6 @@ app.post(
       });
     }
 
-    /*     if (persons.some((p) => p.name === body.name)) {
-      return response.status(400).json({
-        error: `${body.name} already exists in phonebook`,
-      });
-    } */
-
-    /*     const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    }; */
     const person = new Person({
       name: body.name,
       number: body.number,
@@ -97,10 +60,9 @@ app.post(
 );
 
 app.delete("/api/persons/:id", morgan("tiny"), (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 const PORT = process.env.PORT;
@@ -108,4 +70,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// time spent: 14 hrs
+// time spent: 15 hrs
